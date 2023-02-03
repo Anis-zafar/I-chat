5ca4a2ecbdd9e138b80f2e0277f16f8411bf3050
anis-zafar <anis.zafar@ceative.co.uk>
2023-02-03 20:40:30 +0500

    new commit

--- temp.js
+++ temp.js
@@ -23,22 +23,22 @@
 //         .join("")}-${numbers.slice(6).join("")}`);
 // }
 // createPhoneNumber(arr);
-let start = Date.now();
-let arr1 = [
-  1, 4, 6, 3, 9, 8, 8, 8, 8, 8, 0, 1, 1, 1, 10, 10, 10, 10, 11, 11, 11,
-];
-const arr = Array.from(new Set(arr1)).sort(function (a, b) {
-  return a - b;
-});
-const check = (arr) => {
-  const a = Math.max(arr);
-  arr.pop(a);
-};
-for (let a = 0; a <= 1; a++) {
-  check(arr); 
-}
-const thirdmax = Math.max(...arr);
-console.log(thirdmax);
+// let start = Date.now();
+// let arr1 = [
+//   1, 4, 6, 3, 9, 8, 8, 8, 8, 8, 0, 1, 1, 1, 10, 10, 10, 10, 11, 11, 11,
+// ];
+// const arr = Array.from(new Set(arr1)).sort(function (a, b) {
+//   return a - b;
+// });
+// const check = (arr) => {
+//   const a = Math.max(arr);
+//   arr.pop(a);
+// };
+// for (let a = 0; a <= 1; a++) {
+//   check(arr);
+// }
+// const thirdmax = Math.max(...arr);
+// console.log(thirdmax);
 // const fun = (arr, num) => {
 //   const minNum = Array.from(new Set(arr)).sort((a, b) => {
 //     return a - b;
@@ -47,5 +47,31 @@ console.log(thirdmax);
 // };
 // console.log(fun(arr1, 5));
 
-let timeTaken = Date.now() - start;
-console.log("Total time taken : " + timeTaken + " milliseconds");
+// let timeTaken = Date.now() - start;
+// console.log("Total time taken : " + timeTaken + " milliseconds");
+
+const fucntion = (number) => {
+  const arr1 = Array.from(String(number), Number);
+  arr1
+    .sort(function (a, b) {
+      return a - b;
+    })
+    .reverse();
+  const num = arr1.join("");
+  console.log(Math.floor(num));
+  // console.log(arr1);
+  // console.log(num1);
+  console.log(typeof num);
+};
+
+fucntion(1598765423);
+
+// const fucntion1 = (number) => {
+//   const arr1 = String(number).split('').map(number);
+//   arr1.sort(function (a, b) {
+//     return a - b;
+//   });
+//   console.log(arr1);
+// };
+
+// fucntion(1266554433228811);
